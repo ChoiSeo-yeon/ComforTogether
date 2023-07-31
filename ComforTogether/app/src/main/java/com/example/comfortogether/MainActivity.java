6a34c718a7cf1cd02d5fc73a969001c29e992687
ChoiSeo-yeon <49613730+ChoiSeo-yeon@users.noreply.github.com>
2023-07-31 17:42:18 +0900

    bb
    bb

--- ComforTogether/app/src/main/java/com/example/comfortogether/MainActivity.java
+++ ComforTogether/app/src/main/java/com/example/comfortogether/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         int a = 0;
+        int b=0;
 
         go_play_btn = findViewById(R.id.go_play_btn);
         go_tuto_btn = findViewById(R.id.go_tuto_btn);
